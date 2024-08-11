996a4cf6d95e564923c4ae7e0e2ec8f56a427ad7
Huillca Reynaldo <lymreynaldo@icloud.com>
2024-08-11 19:07:05 -0300

    chore: remove layered architecture

--- src/main/java/uk/layme/cleanarchitecture/model/User.java
+++
@@ -1,9 +0,0 @@
-package uk.layme.cleanarchitecture.model;
-
-import org.springframework.data.annotation.Id;
-
-public record User (@Id Long id,
-                    String username,
-                    String password,
-                    String email) {
-}
